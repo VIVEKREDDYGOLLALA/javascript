ca0fef4628d4c3d21fbeee1b1375421fcee33013
VIVEKREDDY <141652923+VIVEKREDDYGOLLALA@users.noreply.github.com>
2023-12-22 20:42:43 +0530

    Add files via upload
    added save and revised data functions

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-
-
 let countEl=document.getElementById("count-el")
+let saaveEl=document.getElementById("save-el")
 let count=0;
 function increment() {
     count=count+1;  
     console.log(countEl)
-    countEl.innerText=count
+    countEl.innerText=count; 
+}
+function save(){    
+    let countstr=count+" - ";
+    saaveEl.textContent+=countstr
+    console.log(count);
+    countEl.innerText=0; 
+    count=0
 }
